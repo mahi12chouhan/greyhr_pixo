92e8d2aeba4c0164516b42c0084e71c67c062dfa
mahima <mahima.c@pixoatic.com>
2023-06-16 19:46:57 +0530

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/admin/login', to: 'active_admin/devise/sessions#new'
+  # get '/admin/login', to: 'active_admin/devise/sessions#new'
   post '/login', to: 'accounts#login'
   post '/forgot_password', to: 'accounts#forgot_password'
   post '/reset_password', to: 'accounts#reset_password'
